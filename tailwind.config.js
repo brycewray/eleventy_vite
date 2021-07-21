5232a7f1a2f6a4eb98d943c871a3460da00fdab2
Bryce Wray <bwray@wraytx.com>
2021-07-21 16:40:08 -0500

    Adding font-family stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,11 @@ module.exports = {
         '900': '#000066',
       },
     },
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', 'Noto Sans', 'Segoe UI', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', 'Times', 'Times New Roman', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'SF Mono', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+    },
   },
   variants: {},
   plugins: [], // if we add forms, do it here
