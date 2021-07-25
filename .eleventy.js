6bda4c17a8b711c57f0519dbf32e68d161c1bc62
Bryce Wray <bwray@wraytx.com>
2021-07-25 08:29:57 -0500

    Restoring safe parts of Eleventy/BSync config

--- .eleventy.js
+++ .eleventy.js
@@ -168,11 +168,36 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary("md", markdownEngine)
   /* --- end, Markdown handling --- */
 
-
   eleventyConfig.addWatchTarget("src/**/*.js")
   eleventyConfig.addWatchTarget("./src/assets/css/*.css")
   // eleventyConfig.addWatchTarget("./src/assets/scss/*.scss")
   eleventyConfig.addWatchTarget("./src/**/*.md")
+
+  eleventyConfig.setBrowserSyncConfig({
+    ...eleventyConfig.browserSyncConfig,
+    files: [
+      "src/**/*.js", 
+      "src/assets/css/*.css", 
+      // "src/assets/scss/*.scss", 
+      "src/**/*.md"
+    ],
+    ghostMode: false,
+    // port: 3000, // conflicts with Vite
+    /*
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html')
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          res.end()
+        })
+      }
+    },*/
+    snippet: false,
+  })
 
   eleventyConfig.addShortcode(
     "imgc",
