d570cd9b1ed53ea7852d2639665d70813d932a41
Bryce Wray <bwray@wraytx.com>
2021-07-25 16:24:41 -0500

    Removing possibly problematic code

--- .eleventy.js
+++ .eleventy.js
@@ -173,32 +173,6 @@ module.exports = function (eleventyConfig) {
   // eleventyConfig.addWatchTarget("./src/assets/scss/*.scss")
   eleventyConfig.addWatchTarget("./src/**/*.md")
 
-  eleventyConfig.setBrowserSyncConfig({
-    ...eleventyConfig.browserSyncConfig,
-    files: [
-      "src/**/*.js", 
-      "src/assets/css/*.css", 
-      // "src/assets/scss/*.scss", 
-      "src/**/*.md"
-    ],
-    ghostMode: false,
-    // port: 3000, // conflicts with Vite
-    /*
-    callbacks: {
-      ready: function(err, bs) {
-        bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync('_site/404.html')
-          // Add 404 http status code in request header.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
-          // Provides the 404 content without redirect.
-          res.write(content_404)
-          res.end()
-        })
-      }
-    },*/
-    snippet: false,
-  })
-
   eleventyConfig.addShortcode(
     "imgc",
     require("./src/assets/utils/imgc.js")
