68c2566a4f2c1159c4126fd190cb4a6750dd56b4
Bryce Wray <bwray@wraytx.com>
2021-07-27 07:50:58 -0500

    Import font file (with more generic name)

--- src/client/main.js
+++ src/client/main.js
@@ -3,4 +3,6 @@ import "../assets/css/index.css"
 import "../assets/js/lazyload_17-4-0.min.js"
 import "../assets/js/lazyload-helper.js"
 import "../assets/js/mobile-menu-helper.js"
-
+// from here, see https://github.com/vitejs/vite/issues/2375
+import font from "../assets/fonts/Inter-VF_subset.woff2"
+console.log(font)
