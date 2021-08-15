7b0132b009e579833e28c331ea44621dcbbc4c4f
Bryce Wray <bwray@wraytx.com>
2021-08-14 19:57:14 -0500

    Rename lazyload file to track versions

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,6 @@
 import "vite/dynamic-import-polyfill"
 import "../assets/css/index.css"
-import "../assets/js/lazyload.min.js"
+import "../assets/js/lazyload_17-4-0.min.js"
 import "../assets/js/lazyload-helper.js"
 import "../assets/js/mobile-menu-helper.js"
 // from here, see https://github.com/vitejs/vite/issues/2375
